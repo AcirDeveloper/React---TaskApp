5df6e3bfd292efd0f2fc4a8ca769966d2a5c7bc9
AcirDeveloper <acirdeveloper@gmail.com>
2022-08-16 02:22:27 -0500

    fracaso

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const taskRoutes = require("./routes/tasks.routes");
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const taskRoutes = require('./routes/tasks.routes')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(taskRoutes);
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(taskRoutes)
 app.use((err, req, res, next) => {
-    return res.json({
-        message: err.message,
-    });
-});
+	return res.json({
+		message: err.message,
+	})
+})
 
-app.listen(4000);
-console.log("Server on port 4000");
+app.listen(4000)
+console.log('Server on port 4000')
